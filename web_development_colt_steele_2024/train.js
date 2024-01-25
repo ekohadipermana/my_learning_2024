fbdbe5fce82bd4d85f755ef54d688a126b9bb315
ekohadipermana <ekohadipermana@gmail.com>
2024-01-25 17:57:43 +0700

    creating first fibonacci

--- web_development_colt_steele_2024/train.js
+++ web_development_colt_steele_2024/train.js
@@ -13,3 +13,16 @@ for (var i = 0; i < 2; i++) {
 }
 
 // is this a comment?
+
+// The Fibonacci sequence
+// 0, 1, 1, 2, 3, 5, 8, 13, 21
+// n0, n1
+// n2 = n0 + n1
+
+var fibSeq = [0, 1];
+var n0 = fibSeq[c0];
+var n1 = fibSeq[c1];
+var n2 = n0 + n1;
+c0++;
+c1++;
+
