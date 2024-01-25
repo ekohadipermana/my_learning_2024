f9fd3e20990b262b280e967aa91b8cf0981efc9a
ekohadipermana <ekohadipermana@gmail.com>
2024-01-25 18:41:53 +0700

    creating complete code

--- web_development_colt_steele_2024/train.js
+++ web_development_colt_steele_2024/train.js
@@ -19,6 +19,7 @@ for (var i = 0; i < 2; i++) {
 // n0, n1
 // n2 = n0 + n1
 
+// Original code
 var fibSeq = [0, 1];
 var c0 = 0;
 var c1 = 1;
@@ -29,3 +30,21 @@ c0++;
 c1++;
 fibSeq.push(n2);
 console.log(fibSeq);
+// 3 number with 1 step (3-2)
+
+var qty = prompt('How many Fibonacci do you want?');
+var fibSeq = [0, 1];
+var c0 = 0;
+var c1 = 1;
+while (c1 < qty){
+    var n0 = fibSeq[c0];
+    var n1 = fibSeq[c1];
+    var n2 = n0 + n1;
+    c0++;
+    c1++;
+    fibSeq.push(n2);
+}
+
+console.log(fibSeq);
+
+// 
