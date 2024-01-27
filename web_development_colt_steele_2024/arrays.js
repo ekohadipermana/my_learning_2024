f36b9ff532b8ff41a4489f30c8b7d31200ffc0b4
ekohadipermana <ekohadipermana@gmail.com>
2024-01-27 08:55:46 +0700

    modifying for arrays

--- web_development_colt_steele_2024/arrays.js
+++ web_development_colt_steele_2024/arrays.js
@@ -1,2 +1,5 @@
 let numArray = [3, 5, 7, 11, 13, 15];
-console.log(numArray.slice(0, 3));
+console.log(numArray.slice(1, 4));
+let learningTime = ['04.30', '05.30', '08.30', '09.40', '10.50', '12.00'];
+let learningTimeDescription = ['Dawn Learning', 'Morning Learning', 'Evening Learning'];
+console.log(`Learning at dawn begin at ${learningTime[0]}`);
