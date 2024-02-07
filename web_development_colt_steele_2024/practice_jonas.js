55c4cb6f8dee9fac0d32b8c654d2c2ea4c58527a
ekohadipermana <ekohadipermana@gmail.com>
2024-02-07 22:02:11 +0700

    if else practice

--- web_development_colt_steele_2024/practice_jonas.js
+++ web_development_colt_steele_2024/practice_jonas.js
@@ -22,12 +22,36 @@ with
 a template literal`);
 
 // testing
-*/
+
 
 let customerAge = 30;
 if (customerAge >= 18) {
     console.log('You are adult ');
     console.log('You are legal');
 }
+*/
+
+let massMark;
+let heightMark;
+let massjohn;
+let heightJohn;
+massMark = 78;
+heightMark = 1.69;
+massjohn = 92;
+heightJohn = 1.95;
+const bmiMark = Math.round(massMark / (heightMark ** 2));
+const bmiJohn = Math.round(massjohn / (heightJohn ** 2));
+
+if (bmiJohn > bmiMark) {
+    console.log(`John's BMI is ${bmiJohn}, higher than Mark's (${bmiMark})`);
+}
+else {
+    console.log(`Mark's BMI is ${bmiMark}, higher than John's (${bmiJohn})`);
+}
+
+// console.log(bmiMark, bmiJohn);
+// let markHigherBMI = bmiMark > bmiJohn;
+// console.log(markHigherBMI);
+
 
 
