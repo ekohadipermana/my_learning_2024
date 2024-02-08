6a5dd48ee48a17661eb745bf77a1d73d5401bf7f
ekohadipermana <ekohadipermana@gmail.com>
2024-02-08 09:15:49 +0700

    commenting out previous practice

--- web_development_colt_steele_2024/practice_jonas.js
+++ web_development_colt_steele_2024/practice_jonas.js
@@ -29,7 +29,7 @@ if (customerAge >= 18) {
     console.log('You are adult ');
     console.log('You are legal');
 }
-*/
+
 
 let massMark;
 let heightMark;
@@ -53,7 +53,29 @@ else {
 // let markHigherBMI = bmiMark > bmiJohn;
 // console.log(markHigherBMI);
 
+*/
+
 // Good morning! I am back!
+
+/* There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+"Dolphins win the trophy" if Dolphins win, or
+
+"Koalas win the trophy" if Koalas win, or
+
+"Both win the trophy" if their average scores are equal.
+
+
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+*/
+
 
 
 
